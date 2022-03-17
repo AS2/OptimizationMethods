61045743088afa05ca8f58351c9232c5d0184a69
AS2 <alex.sachuk@yandex.ru>
2022-03-17 11:45:10 +0300

    Written brute-force solution

---
+++ 2_transport-task/src/src/brute_force.py
@@ -0,0 +1,56 @@
+from itertools import combinations
+import numpy as np
+
+EPS = 0.000000001
+
+def get_basis_matrs(A : np.ndarray):
+    N = A.shape[0]
+    M = A.shape[1]
+
+    basis_matrs = []
+    basis_combinations_indexes = []
+    all_indexes = [i for i in range(M)]
+
+    for i in combinations(all_indexes, N):
+        basis_matr = A[:, i]
+        if np.linalg.det(basis_matr) != 0:
+            basis_matrs.append(basis_matr)
+            basis_combinations_indexes.append(i)
+     
+    return basis_matrs, basis_combinations_indexes
+
+def get_all_possible_vectors(A : list, b : list):
+    N = len(A[0])
+    M = len(A)
+    vectors = []
+
+    if M >= N:
+        return vectors
+    else:
+        basis_matrs, basis_combinations_indexes = get_basis_matrs(np.array(A))
+
+    for i in range(len(basis_matrs)):
+        solve = np.linalg.solve(basis_matrs[i], b)
+        if (len(solve[solve < -1 * EPS]) != 0) or (len(solve[solve > 1e+15]) != 0):
+            continue
+
+        vec = [0 for i in range(N)]
+        for j in range(len(basis_combinations_indexes[i])):
+            vec[basis_combinations_indexes[i][j]] = solve[j]
+        vectors.append(vec)
+    return vectors
+
+def solve_brute_force(A : list, b : list, c : list, v : float):
+    vectors = get_all_possible_vectors(A, b)
+    if len(vectors) == 0:
+        return []
+
+    solution = vectors[0]
+    target_min = np.dot(solution, c)
+
+    for vec in vectors:
+        if np.dot(vec, c) < target_min:
+            target_min = np.dot(vec, c)
+            solution = vec
+
+    return solution
--- 2_transport-task/src/src/src.py
+++ 2_transport-task/src/src/src.py
@@ -4,6 +4,7 @@ from TASK2_2 import *
 
 from utils import *
 from potencial_method import *
+from brute_force import *
 
 # работаем с введенными из TASK.py переменными:
 #                 M, N, m_count, n_count, coefs
@@ -32,6 +33,12 @@ print_result(poten_sol, coefs22)
 
 
 # TASK4: РЕШИТЬ ТУ ЖЕ ЗАДАЧУ МЕТОДОМ ПЕРЕБОРА КРАЙНИХ ТОЧЕК
+print("Brute force solution")
 M, N, m_count, n_count, coefs = make_task_close(M, N, m_count, n_count, coefs)
 min_task, A_matr, b_vec = to_canon(M, N, m_count, n_count, coefs)
-# тут уже кодик с канон задачей и переборе как а-ля 1 лаба
+# удалить последнюю строчку
+A_matr.pop(len(A_matr) - 1)
+b_vec.pop(len(b_vec) - 1)
+brute_solution =solve_brute_force(A_matr, b_vec, min_task, 0)
+brute_solution = np.reshape(brute_solution, (M, N))
+print_result(brute_solution, coefs)
