a2d3fc417920b3a9f9d08589cf37114417d6d396
AS2 <alex.sachuk@yandex.ru>
2022-04-17 17:33:47 +0300

    Added somegraphic results

--- 4_two-dim-min/task4-src/task3_src.py
+++ 4_two-dim-min/task4-src/task3_src.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
 import numpy as np
@@ -12,20 +13,21 @@ def taskPlot():
     x = np.arange(-4, 1, 0.05)
     y = np.arange(-4, 1, 0.05)
     xgrid, ygrid = np.meshgrid(x, y)
-
     zgrid = 2*(xgrid**2) + ygrid**2 + np.cos(xgrid + ygrid) + 3 * xgrid + 4 * ygrid
 
     fig = plt.figure(figsize=(7, 4))
     ax_3d = Axes3D(fig)
 
-    ax_3d.plot_surface(xgrid, ygrid, zgrid, rstride=5, cstride=5, cmap='plasma')
+    #  Создаем массив RGB цветов каждой области:
+    ax_3d.plot_surface(xgrid, ygrid, zgrid, rstride=5, cstride=5, cmap=cm.jet)
     plt.show()
     return
 
-def printResult(steps : list, iters : int, eps : float, methodName : str):
+def printResult(steps : list, iters : int, fres : float, eps : float, methodName : str):
     print(methodName)
     print("Eps: " + str(eps))
     print("Res: " + str(steps[len(steps) - 1]))
+    print("f(Res): " + str(fres))
     print("Iters: " + str(iters))
     return
 
@@ -43,14 +45,14 @@ def plotSteps(steps : list):
     xSteps = [step[0] for step in steps]
     ySteps = [step[1] for step in steps]
     #plt.title("Line levels of '")
-    plt.plot(xSteps, ySteps, "b--")
+    plt.plot(xSteps, ySteps, "bo--")
     cs = plt.contour(x, y, z)
     plt.clabel(cs)
     plt.show()
     return
 
 def main():
-    EPS = [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001]
+    EPS = [0.1, 0.01, 0.001]
     currentTask = t.Task()
 
     taskPlot()
@@ -59,16 +61,21 @@ def main():
 
     for ep in EPS:
         steps, iters = gradWFragStep.gradWFragStepSolver(ep)
-        printResult(steps, iters, ep, "GRADIENT WITH FRAG STEP")
+        printResult(steps, iters, currentTask.f(steps[len(steps) - 1]), ep, "GRADIENT WITH FRAG STEP")
         if (ep == EPS[len(EPS) - 1]):
             plotSteps(steps)
 
     # Alghoritm - https://habr.com/ru/post/333356/
     for ep in EPS:
         steps, iters = grafBFGS.grafBFGSSolver(ep)
-        printResult(steps, iters, ep, "BFGS METHOD")
+        printResult(steps, iters, currentTask.f(steps[len(steps) - 1]), ep, "BFGS METHOD")
         if (ep == EPS[len(EPS) - 1]):
             plotSteps(steps)
+
+    steps, iters = gradWFragStep.gradWFragStepSolver(0.000001)
+    print(steps[len(steps) - 1])
+    print(m.sin(steps[len(steps) - 1][0] + steps[len(steps) - 1][1]))
+    print(currentTask.grad_f(steps[len(steps) - 1]))
 
 
 main()
